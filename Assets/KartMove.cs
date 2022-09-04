4b0f9cba90cb2cc4ee881d676a132367c9d1c573
hktrollgames <hktrollgames@gmail.com>
2022-09-04 17:48:56 +0900

    이동 로직 물리 로직으로 수정

--- Assets/KartMove.cs
+++ Assets/KartMove.cs
@@ -1,18 +1,46 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class KartMove : MonoBehaviour
 {
-    public float speed = 5;
-    public float rotate = 100;
-    void Update()
+    public float acc = 10000f;      // 가속도(acceleration)
+    public float maxSpeed = 10000f;
+    public float currentSpeed = 0;
+    public float attenuation = 100;   // 마찰에 의한 속도 감쇠('감쇠'는 '힘이나 세력 따위가 줄어서 약하여짐.')
+    [SerializeField] float maxAttenuation;
+    public float backSpeedRatio = 0.5f;
+    public float rotate = 1f;
+    public Rigidbody rb;
+    private void FixedUpdate()
     {
-        float forwardMove = Input.GetAxis("Vertical")
-            * speed * Time.deltaTime; // s 뒤로  w앞으로
-        transform.Translate(0, 0, forwardMove);
-        float _rotate = Input.GetAxis("Horizontal") 
-            * rotate * Time.deltaTime;
-        transform.Rotate(new Vector3(0, _rotate, 0));
+        var angularVelocity = rb.angularVelocity;
+        angularVelocity.x = 0; // 앞뒤로 흔들리는 물리 제거
+        angularVelocity.z = 0; // 좌우로 흔들리는 물리 제거
+        rb.angularVelocity = angularVelocity;
+
+        float forwardMove = Input.GetAxis("Vertical");
+
+        currentSpeed = currentSpeed + acc * forwardMove;  // 속도 증가/감소
+        float absCurrentSpeed = Math.Abs(currentSpeed);
+        maxAttenuation = Math.Clamp(acc * attenuation, -absCurrentSpeed, absCurrentSpeed);
+        currentSpeed = currentSpeed + (currentSpeed > 0 ? -maxAttenuation : maxAttenuation); // 감쇠
+        currentSpeed = Math.Clamp(currentSpeed, -maxSpeed * backSpeedRatio, maxSpeed);
+        float force = currentSpeed;
+
+        if (force != 0)
+        {
+            var velocity = rb.velocity;
+            velocity.x = 0;
+            velocity.z = 0;
+            rb.velocity = velocity;
+            var forward = transform.forward;
+            forward.y = 0;
+            rb.AddForce(forward * force);
+        }
+
+        float rotate = Input.GetAxis("Horizontal") * this.rotate;
+        rb.AddRelativeTorque(0, rotate, 0, ForceMode.VelocityChange);
     }
 }
