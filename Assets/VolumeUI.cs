03ef2e1163019fcfe3186e7e3fd7dd2973f8259f
hktrollgames <hktrollgames@gmail.com>
2022-09-04 15:38:29 +0900

    볼륨조절 슬라이더 추가

---
+++ Assets/VolumeUI.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class VolumeUI : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void ChangeBGMVolume(float volume) => audioMixer.SetFloat("BGM", volume);
+    public void ChangeSFXVolume(float volume) => audioMixer.SetFloat("SFX", volume);
+}
