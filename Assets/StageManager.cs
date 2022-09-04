0316b6a8d066e04d8e21a46fe7b34362c055f4fc
hktrollgames <hktrollgames@gmail.com>
2022-09-04 16:38:36 +0900

    시작 카운트 다운 표시

---
+++ Assets/StageManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class StageManager : MonoBehaviour
+{
+    public TMP_Text countDownText;
+    public int remainTime = 3;
+    public AudioSource audioSource;
+    public AudioClip countDownClip;
+    public AudioClip startClip;
+    public AudioClip earlyStartClip;
+    public KartMove kartMove;
+    private IEnumerator Start()
+    {
+        var waitTime = new WaitForSeconds(1);
+        kartMove.enabled = false;
+        while (remainTime > 0)
+        {
+            countDownText.text = remainTime.ToString(); // 3
+            remainTime--;
+            audioSource.PlayOneShot(countDownClip);
+            yield return waitTime;
+            //yield return new WaitForSeconds(1); // 위와 같은 의미.
+        }
+
+        countDownText.text = "Start!";
+        kartMove.enabled = true;
+        audioSource.PlayOneShot(startClip);
+    }
+
+    float nextEnableTime;
+    private void Update()
+    {
+        if (kartMove.enabled)
+            return;
+
+        if (nextEnableTime > Time.time)
+            return;
+
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            audioSource.PlayOneShot(earlyStartClip);
+            nextEnableTime += 2;
+        }
+    }
+}
---
+++ Assets/TextureAnimation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureAnimation : MonoBehaviour
+{
+    public Texture2D[] textures;
+    public float delay = 0.07f;
+    private IEnumerator Start()
+    {
+        var mat = GetComponent<Renderer>().material;
+        int i = 0;
+        while(true)
+        {
+            mat.mainTexture = textures[i];
+            yield return new WaitForSeconds(delay);
+            i++;
+            if (textures.Length - 1 == i)
+                i = 0;
+        }
+    }
+}
